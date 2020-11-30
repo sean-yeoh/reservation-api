33272e0574e7d00e0ca76b035533a180b7520499
Sean Yeoh <sean.yeoh@hotmail.com>
2020-11-30 13:40:47 +0800

    Add monetize gem

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+  monetize :payout_price_cents, with_model_currency: :currency
+  monetize :security_price_cents, with_model_currency: :currency
+  monetize :total_price_cents, with_model_currency: :currency
 end
